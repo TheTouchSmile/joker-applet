1ff3e3da67a2d235cb069e6c30459be9688e434f
xuchihua <xuchihua@yeah.net>
2018-03-26 00:32:39 +0800

    小程序端支付

---
+++ src/test/java/com/joker/JokerAppletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JokerAppletApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
